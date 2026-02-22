f2ba15cbbdc15c32f82c13bb3c98e15240516ea5
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2026-02-22 09:16:42 +0530

    LearningRestApis and then and notes

---
+++ LearningRestApis/src/test/java/com/sahay/aayush/LearningRestApis/LearningRestApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahay.aayush.LearningRestApis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningRestApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
