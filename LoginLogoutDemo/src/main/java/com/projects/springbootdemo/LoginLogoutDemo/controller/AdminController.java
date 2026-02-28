baf19239b57aabe32438a0ca4ff6daa30f7c1c6b
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2026-02-28 13:55:16 +0530

    implemented login, logout using session

--- LoginLogoutDemo/src/main/java/com/projects/springbootdemo/LoginLogoutDemo/controller/AdminController.java
+++ LoginLogoutDemo/src/main/java/com/projects/springbootdemo/LoginLogoutDemo/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.projects.springbootdemo.LoginLogoutDemo.controller;
 
 import com.projects.springbootdemo.LoginLogoutDemo.dto.AdminLoginRequestDto;
 import com.projects.springbootdemo.LoginLogoutDemo.service.AdminService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -31,6 +32,7 @@ public class AdminController {
         return "redirect:/AdminDashboard.html";
     }
  */
+
 @Controller
 @RequiredArgsConstructor
 public class AdminController {
@@ -41,6 +43,8 @@ public class AdminController {
         return "forward:/AdminLogin.html";
     }
 
+/* Second Update that used no session [Exclude this section incase printing]
+-----------------------------------------------------------------------------
     @PostMapping("/login")
     public String login(@Valid @ModelAttribute AdminLoginRequestDto request,
                         BindingResult result) {
@@ -60,6 +64,53 @@ public class AdminController {
 
         return "redirect:/AdminDashboard.html";
     }
+-----------------------------------------------------------------------------
+*/
+@PostMapping("/login")
+public String login(@Valid @ModelAttribute AdminLoginRequestDto request,
+                    BindingResult result,
+                    HttpSession session) {
+
+    if (result.hasErrors()) {
+        return "redirect:/AdminLogin.html";
+    }
+
+    boolean isValid = adminService.authenticate(
+            request.getEmail(),
+            request.getPassword()
+    );
+
+    if (!isValid) {
+        return "redirect:/AdminLogin.html";
+    }
+
+
+    session.setAttribute("loggedInAdmin", request.getEmail());
+
+    return "redirect:/AdminDashboard.html";
+}
+
+//-----------Dashboard Code Url Mapping Start----------------------
+@GetMapping("/dashboard")
+public String dashboard(HttpSession session) {
+
+    if (session.getAttribute("loggedInAdmin") == null) {
+        return "redirect:/";
+    }
+
+    return "forward:/AdminDashboard.html";
+}
+//-----------Dashboard Code Url Mapping End----------------------
+
+//-----------Implementation of the Code of Logout  Start ---------------
+@GetMapping("/logout")
+public String logout(HttpSession session) {
+
+    session.invalidate();   // destroys session
+
+    return "redirect:/";
+}
+//-----------Implementation of the Code of Logout  End ---------------
 
 }
 
