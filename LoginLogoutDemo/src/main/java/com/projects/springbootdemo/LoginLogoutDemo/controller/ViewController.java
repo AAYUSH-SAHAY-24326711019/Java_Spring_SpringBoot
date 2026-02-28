032489fe159a75535951e7a3b074032820b9dde9
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2026-02-28 12:31:41 +0530

    done url mapping for the AdminLogin page

---
+++ LoginLogoutDemo/src/main/java/com/projects/springbootdemo/LoginLogoutDemo/controller/ViewController.java
@@ -0,0 +1,12 @@
+package com.projects.springbootdemo.LoginLogoutDemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+    @GetMapping("/")
+    public String loginPage() {
+        return "forward:/AdminLogin.html";
+    }
+}
